d5769c6c236b6d205ffe3dad414d7a77060d4ec2
kacperdubiel <kacper.dubiel13@gmail.com>
2021-09-12 16:33:56 +0200

    Initialized backend and frontend services

---
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/WellbeeingApplication.java
@@ -0,0 +1,13 @@
+package com.wellbeeing.wellbeeing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellbeeingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellbeeingApplication.class, args);
+	}
+
+}
---
+++ backend-springboot/src/test/java/com/wellbeeing/wellbeeing/WellbeeingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellbeeing.wellbeeing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellbeeingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
