41b18ac5b1d30366c492cd1ac5844e0c79187d74
majajurenczyk <majka.jurenczyk@gmail.com>
2021-10-10 19:34:32 +0200

    spring security basic configuration

---
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/config/SpringSecurityConfig.java
@@ -0,0 +1,63 @@
+package com.wellbeeing.wellbeeing.config;
+
+import com.wellbeeing.wellbeeing.service.UserService;
+import com.wellbeeing.wellbeeing.util.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private UserService userService;
+    private JwtRequestFilter jwtRequestFilter;
+
+    protected SpringSecurityConfig(@Qualifier("userService") UserService userService,
+                                   @Qualifier("jwtRequestFilter") JwtRequestFilter jwtRequestFilter) {
+        super();
+        this.userService = userService;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate", "/register")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
