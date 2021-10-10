4d6ab5f0527e482d28476e562cce2e88b1cc8b59
majajurenczyk <majka.jurenczyk@gmail.com>
2021-10-10 19:34:09 +0200

    user service added

---
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/service/UserService.java
@@ -0,0 +1,33 @@
+package com.wellbeeing.wellbeeing.service;
+
+import com.wellbeeing.wellbeeing.domain.User;
+import com.wellbeeing.wellbeeing.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserService implements UserDetailsService, UserServiceApi {
+    private UserDAO userDAO;
+
+    public UserService(@Qualifier("userDAO") UserDAO userDAO){
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User foundUser = userDAO.findUserByEmail(email).orElse(null);
+        if(foundUser != null)
+            return foundUser;
+        else throw new UsernameNotFoundException("User: " + email + " not found");
+    }
+
+    @Override
+    public boolean register(User user) {
+        User userToRegister = new User(user.getUsername(), user.getPassword());
+        userDAO.save(userToRegister);
+        return true;
+    }
+}
---
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/service/UserServiceApi.java
@@ -0,0 +1,7 @@
+package com.wellbeeing.wellbeeing.service;
+
+import com.wellbeeing.wellbeeing.domain.User;
+
+public interface UserServiceApi {
+    public boolean register(User user);
+}
