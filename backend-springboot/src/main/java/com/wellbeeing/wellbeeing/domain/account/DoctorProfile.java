5ee360a428c0c308d08ccc568f08a65bf05ebc8f
kacperdubiel <kacper.dubiel13@gmail.com>
2021-10-15 22:31:12 +0200

    Added doctor specialization

--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/account/DoctorProfile.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/account/DoctorProfile.java
@@ -1,11 +1,10 @@
 package com.wellbeeing.wellbeeing.domain.account;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.UUID;
+import javax.persistence.*;
 
 @Entity
 public class DoctorProfile extends SpecialistProfile {
+    @Column(nullable = false)
+    private EDoctorSpecialization specialization;
+
 }
