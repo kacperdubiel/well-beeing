673be871f318dd47e7c8514eb85c44df72d932f4
Student246752 <246752@student.pwr.edu.pl>
2021-10-15 21:58:17 +0200

    Add exercise controller

---
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/api/sport/ExerciseController.java
@@ -0,0 +1,9 @@
+package com.wellbeeing.wellbeeing.api.sport;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class ExerciseController {
+}
