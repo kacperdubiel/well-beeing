0cfeb67eb0d75f8a937045b46b0092a6b6bf678e
majajurenczyk <majka.jurenczyk@gmail.com>
2021-10-16 00:12:45 +0200

    any and ranges added

--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/diet/EGlycemicIndexLevel.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/diet/EGlycemicIndexLevel.java
@@ -4,10 +4,14 @@ package com.wellbeeing.wellbeeing.domain.diet;
 public enum  EGlycemicIndexLevel {
         LOW(1, 55),
         MEDIUM(56, 69),
-        HIGH(70, Integer.MAX_VALUE)
-    ;
+        HIGH(70, Integer.MAX_VALUE),
+        ANY(1, Integer.MAX_VALUE );
 
+    private int lowerRange;
+    private int higherRange;
 
     EGlycemicIndexLevel(int lower_range, int higher_range) {
+        this.lowerRange = lower_range;
+        this.higherRange = higher_range;
     }
 }
