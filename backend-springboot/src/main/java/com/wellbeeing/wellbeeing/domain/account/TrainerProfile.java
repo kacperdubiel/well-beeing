b98ac923edb848fbdc08529acfcd089f820ed2bf
Student246752 <246752@student.pwr.edu.pl>
2021-10-16 15:21:13 +0200

    Fix

--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/account/TrainerProfile.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/account/TrainerProfile.java
@@ -1,8 +1,10 @@
 package com.wellbeeing.wellbeeing.domain.account;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.UUID;
-
+@NoArgsConstructor
 @Entity
 public class TrainerProfile extends SpecialistProfile {
 }
