c738fa0300275cb44a182221baa2edf6fac0089a
kacperdubiel <kacper.dubiel13@gmail.com>
2021-10-16 19:22:38 +0200

    Added measures and profile connections link to profile

--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/account/Profile.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/account/Profile.java
@@ -1,5 +1,8 @@
 package com.wellbeeing.wellbeeing.domain.account;
 
+import com.wellbeeing.wellbeeing.domain.telemedic.Measure;
+import com.wellbeeing.wellbeeing.domain.telemedic.ProfileConnection;
+
 import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
@@ -22,4 +25,100 @@ public abstract class Profile {
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "profile_card_id", referencedColumnName = "id")
     private ProfileCard profileCard;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Measure> measures;
+
+    @OneToMany(mappedBy = "profile")
+    private List<ProfileConnection> profileConnections;
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getProfileImgPath() {
+        return profileImgPath;
+    }
+
+    public void setProfileImgPath(String profileImgPath) {
+        this.profileImgPath = profileImgPath;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public com.wellbeeing.wellbeeing.domain.account.ESex getESex() {
+        return ESex;
+    }
+
+    public void setESex(com.wellbeeing.wellbeeing.domain.account.ESex ESex) {
+        this.ESex = ESex;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public User getProfileUser() {
+        return profileUser;
+    }
+
+    public void setProfileUser(User profileUser) {
+        this.profileUser = profileUser;
+    }
+
+    public ProfileCard getProfileCard() {
+        return profileCard;
+    }
+
+    public void setProfileCard(ProfileCard profileCard) {
+        this.profileCard = profileCard;
+    }
+
+    public List<Measure> getMeasures() {
+        return measures;
+    }
+
+    public void setMeasures(List<Measure> measures) {
+        this.measures = measures;
+    }
+
+    public List<ProfileConnection> getProfileConnections() {
+        return profileConnections;
+    }
+
+    public void setProfileConnections(List<ProfileConnection> profileConnections) {
+        this.profileConnections = profileConnections;
+    }
+
 }
