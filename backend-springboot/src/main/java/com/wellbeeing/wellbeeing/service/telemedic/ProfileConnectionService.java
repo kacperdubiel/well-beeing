21466884e3dbb77d0b7f7a59c9c5a475fcec4544
kacperdubiel <kacper.dubiel13@gmail.com>
2021-10-26 22:04:24 +0200

    Fixed profile connections system, added pagination, updated security

--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/service/telemedic/ProfileConnectionService.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/service/telemedic/ProfileConnectionService.java
@@ -1,18 +1,23 @@
 package com.wellbeeing.wellbeeing.service.telemedic;
 
 import com.wellbeeing.wellbeeing.domain.account.Profile;
+import com.wellbeeing.wellbeeing.domain.exception.ConflictException;
+import com.wellbeeing.wellbeeing.domain.exception.NotFoundException;
 import com.wellbeeing.wellbeeing.domain.telemedic.EConnectionType;
 import com.wellbeeing.wellbeeing.domain.telemedic.ProfileConnection;
+import org.springframework.data.domain.Page;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface ProfileConnectionService {
-    ProfileConnection getProfileConnectionById(UUID profileConnectionId);
-    List<ProfileConnection> getProfileConnectionsByProfile(Profile profile);
-    List<ProfileConnection> getProfileConnectionsByProfileAndType(Profile profile, EConnectionType connectionType);
-    ProfileConnection getProfileConnectionByProfileAndConnectedWithAndType(Profile profile, Profile connectedWith, EConnectionType connectionType);
-    ProfileConnection addProfileConnection(ProfileConnection profileConnection);
-    ProfileConnection updateProfileConnection(ProfileConnection updatedProfileConnection);
-    boolean deleteProfileConnectionById(UUID profileConnectionId);
+    ProfileConnection getProfileConnectionById(UUID profileConnectionId) throws NotFoundException;
+    Page<ProfileConnection> getProfileConnectionsByProfileAndTypeAndIsAccepted(
+            Profile profile, EConnectionType connectionType, boolean isAccepted, int page, int size);
+    Page<ProfileConnection> getProfileConnectionsByConnectedWithAndTypeAndIsAccepted(
+            Profile connectedWith, EConnectionType connectionType, boolean isAccepted, int page, int size);
+    ProfileConnection getProfileConnectionByProfileAndConnectedWithAndType(
+            Profile profile, Profile connectedWith, EConnectionType connectionType);
+    ProfileConnection addProfileConnection(ProfileConnection profileConnection) throws ConflictException, NotFoundException;
+    ProfileConnection markAsAccepted(ProfileConnection updatedProfileConnection) throws NotFoundException, ConflictException;
+    void deleteProfileConnectionById(UUID profileConnectionId) throws NotFoundException;
 }
