13420b7493fe2b54d57361735497c774410eebc9
majajurenczyk <majka.jurenczyk@gmail.com>
2021-11-09 08:28:37 +0100

    basic data to import added

--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/util/CsvProductImporter.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/util/CsvProductImporter.java
@@ -1,6 +1,11 @@
 package com.wellbeeing.wellbeeing.util;
 
+import com.wellbeeing.wellbeeing.domain.diet.Ailment;
+import com.wellbeeing.wellbeeing.domain.diet.NutritionLabel;
 import com.wellbeeing.wellbeeing.domain.diet.Product;
+import com.wellbeeing.wellbeeing.domain.diet.type.EAilmentType;
+import com.wellbeeing.wellbeeing.repository.diet.AilmentDAO;
+import com.wellbeeing.wellbeeing.repository.diet.NutritionLabelDAO;
 import com.wellbeeing.wellbeeing.service.diet.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,10 +23,17 @@ import java.util.ArrayList;
 public class CsvProductImporter {
 
     private ProductService productService;
+    private NutritionLabelDAO nutritionLabelDAO;
+    private AilmentDAO ailmentDAO;
+
 
     @Autowired
-    public CsvProductImporter(@Qualifier("productService") ProductService productService){
+    public CsvProductImporter(@Qualifier("productService") ProductService productService,
+                              @Qualifier("nutritionLabelDAO") NutritionLabelDAO nutritionLabelDAO,
+                              @Qualifier("ailmentDAO") AilmentDAO ailmentDAO){
         this.productService = productService;
+        this.nutritionLabelDAO = nutritionLabelDAO;
+        this.ailmentDAO = ailmentDAO;
     }
 
     @Override
@@ -61,10 +73,63 @@ public class CsvProductImporter {
                 numberOfProductsImported++;
                 line = br.readLine();
             }
+            importAilments();
+            importNutritionLabels();
         }
         catch (IOException ioe) {
             ioe.printStackTrace();
         }
         return numberOfProductsImported;
+    }
+    private void importNutritionLabels(){
+        NutritionLabel nl1 = NutritionLabel.builder()
+                .name("KETO")
+                .description("KETO")
+                .build();
+        nutritionLabelDAO.save(nl1);
+        NutritionLabel nl2 = NutritionLabel.builder()
+                .name("WEGAŃSKIE")
+                .description("WEGAŃSKIE")
+                .build();
+        nutritionLabelDAO.save(nl2);
+        NutritionLabel nl3 = NutritionLabel.builder()
+                .name("BEZGLUTENOWE")
+                .description("BEZGLUTENOWE")
+                .build();
+        nutritionLabelDAO.save(nl3);
+        NutritionLabel nl4 = NutritionLabel.builder()
+                .name("WEGETARIAŃSKIE")
+                .description("WEGETARIAŃSKIE")
+                .build();
+        nutritionLabelDAO.save(nl4);
+    }
+    private void importAilments(){
+        Ailment a1 = Ailment.builder()
+                .name("Cukrzyca")
+                .description("Choroba")
+                .isLowGlycemicIndexRecommended(true)
+                .changeInProteinsGramPerKilogram(0)
+                .changeInCalories(0)
+                .type(EAilmentType.ILLNESS)
+                .build();
+        ailmentDAO.save(a1);
+        Ailment a2 = Ailment.builder()
+                .name("Choroba 1")
+                .description("Choroba")
+                .isLowGlycemicIndexRecommended(false)
+                .changeInProteinsGramPerKilogram(0)
+                .changeInCalories(0)
+                .type(EAilmentType.ILLNESS)
+                .build();
+        ailmentDAO.save(a2);
+        Ailment a3 = Ailment.builder()
+                .name("Choroba 2")
+                .description("Choroba")
+                .isLowGlycemicIndexRecommended(false)
+                .changeInProteinsGramPerKilogram(0)
+                .changeInCalories(0)
+                .type(EAilmentType.ILLNESS)
+                .build();
+        ailmentDAO.save(a3);
     }
 }
