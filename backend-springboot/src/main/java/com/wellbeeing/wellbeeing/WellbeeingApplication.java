f9eb59f1aed120be7821ab130d7482d3ac245808
majajurenczyk <majka.jurenczyk@gmail.com>
2021-11-09 04:25:43 +0100

    fixes

--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/WellbeeingApplication.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/WellbeeingApplication.java
@@ -1,32 +1,20 @@
 package com.wellbeeing.wellbeeing;
 
-import com.wellbeeing.wellbeeing.service.files.FileService;
-import com.wellbeeing.wellbeeing.service.sport.ExerciseService;
-import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 @SpringBootApplication
-public class WellbeeingApplication implements WebMvcConfigurer {
+public class WellbeeingApplication /*implements WebMvcConfigurer*/ {
 
-	@Override
+/*	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(new SpecificationArgumentResolver());
 		argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
-	}
+	}*/
 	public static void main(String[] args) {
 		SpringApplication.run(WellbeeingApplication.class, args);
 	}
 
-	@Bean
+/*	@Bean
 	CommandLineRunner init(@Qualifier("fileService") FileService fileService,
 						   @Qualifier("exerciseService")ExerciseService exerciseService) {
 		return (args) -> {
@@ -34,6 +22,6 @@ public class WellbeeingApplication implements WebMvcConfigurer {
 //			fileService.deleteAll();
 //			fileService.init();
 		};
-	}
+	}*/
 
 }
