28f61be28f14341a90156ee4544ca2766f2ad442
kacperdubiel <kacper.dubiel13@gmail.com>
2021-11-14 14:25:28 +0100

    Fixed DoctorSpecializations and MeasureTypes to have unique names

--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/account/DoctorSpecialization.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/account/DoctorSpecialization.java
@@ -12,7 +12,7 @@ public class DoctorSpecialization {
     @GeneratedValue
     private UUID id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     @JsonIgnore
--- backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/telemedic/MeasureType.java
+++ backend-springboot/src/main/java/com/wellbeeing/wellbeeing/domain/telemedic/MeasureType.java
@@ -21,7 +21,7 @@ public class MeasureType {
     @GeneratedValue
     private UUID id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
     @Column(name = "unit")
     private String unit;
