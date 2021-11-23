5a6a03cf9480c7a946c2b26fdf9a30f691111a42
majajurenczyk <majka.jurenczyk@gmail.com>
2021-11-24 00:48:52 +0100

    choosing date for report option added

--- frontend-vue/src/components/diet/reports/ProfileReportsComponent.vue
+++ frontend-vue/src/components/diet/reports/ProfileReportsComponent.vue
@@ -5,11 +5,7 @@
                 <input v-model="this.actualMonth" @change="this.getReportsToShow" class= "form-control" id="month-picker" type="month" name="start" min="2021-01">
             </div>
             <div class="col-lg-7" style="justify-content: flex-end; display: flex; flex-direction: row;">
-                <div v-if="this.showConflictError" class="alert alert-danger alert-dismissible fade show" role="alert">
-                    Raport na dzisiaj już istnieje!
-                    <button @click="clearError" type="button" class="btn-close" aria-label="Close"></button>
-                </div>
-                <button @click="addNewReportForToday" class="add-button"> <font-awesome-icon :icon="['fa', 'plus-circle']"/> </button>
+                <button data-bs-toggle="modal" data-bs-target="#newReportFormModal" class="add-button"> <font-awesome-icon :icon="['fa', 'plus-circle']"/> </button>
             </div>
         </div>
         <table style="color: white; text-align: start;" class="table">
@@ -84,6 +80,34 @@
                 </div>
             </div>
         </div>
+        <div id="newReportFormModal" data-bs-backdrop="static" data-bs-keyboard="false" class="modal fade" tabindex="-1" aria-labelledby="newReportFormModalLabel" aria-hidden="false">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 style="color: black;" class="modal-title" id="newReportFormModalLabel">Dodaj raport</h5>
+                        <button @click="clearStatusAndInput" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div style="text-align: start;">
+                            <p style="color: black;">Wybierz datę raportu: </p>
+                            <input @focus="clearStatus" v-model="this.actualDate" class="form-control" id="date-picker" type="date" format="yyyy-mm-dd" name="start" min="2021-01-01" :max="this.actualDate">
+                            <div style="width: 100%; justify-content: flex-end; display: flex;">
+                                <button @click="addNewReportForDay" style="margin: 2px; align-self: end;" :value="this.getActualLocalDate()" type="button" class="btn-card-panel-diet my-4">Dodaj raport</button>
+                            </div>
+                            <div v-if="this.showConflictError" class="alert alert-danger alert-dismissible fade show my-1" role="alert">
+                                Raport na ten dzień już istnieje!
+                            </div>
+                            <div v-if="this.addReportSuccess" class="alert alert-success alert-dismissible fade show my-1" role="alert">
+                                Dodano raport.
+                            </div>
+                            <div v-if="this.addReportError" class="alert alert-danger alert-dismissible fade show my-1" role="alert">
+                                Wystąpił błąd dodawania raportu. 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -102,6 +126,7 @@ export default {
             reportsToShow: [],
             showConflictError: false,
             actualMonth: '',
+            actualDate: '',
             actualModalReport: {
                 derivedNutritionalValues: {
                     derivedProteins: 0,
@@ -110,26 +135,47 @@ export default {
                     derivedCalories: 0
                 }
             },
-            closeInfoModal: false
+            closeInfoModal: false,
+            addReportSuccess: false,
+            addReportError: false,
         }
     },
     mounted(){
         this.actualMonth = this.getActualMonth()
+        this.actualDate = this.getActualLocalDate()
         this.getReportsToShow()
     },
     methods: {
-        addNewReportForToday(){
+        clearStatus(){
+             this.addReportSuccess = false;
+             this.showConflictError = false;
+             this.addReportError = false;
+        },
+        clearStatusAndInput(){
+            setTimeout(() => {
+                this.clearInput()
+                this.clearStatus()
+            }, 500)
+        },
+        clearInput(){
+            this.actualDate = this.getActualLocalDate()
+        },
+        addNewReportForDay(){
             axios({
                 method: 'post',
                 headers: {Authorization: 'Bearer ' + localStorage.getItem('token')}, 
                 url: "http://localhost:8090/report",
+                data: {
+                    reportDate: this.actualDate
+                }
             })
-            .then((response) => {this.getReportsToShow(); console.log(response)})
+            .then((response) => {this.getReportsToShow(); console.log(response); this.addReportSuccess = true;})
             .catch(error => 
                 {
-                    console.log(error)
+                    this.addReportError = true;
                     if (error.response.status === 409) {
-                        this.showConflictError = true
+                        this.addReportError = false;
+                        this.showConflictError = true;
                     }
                 }
             )},
@@ -162,6 +208,10 @@ export default {
             const month=("0" + (date.getMonth() + 1)).slice(-2)
             const year=date.getFullYear()
             return `${year}-${month}`;
+        },
+        getActualLocalDate(){
+            const date= new Date()
+            return this.$func_global.formatDateDatePicker(date)
         },
         setActualReport(report){
             this.actualModalReport = report
