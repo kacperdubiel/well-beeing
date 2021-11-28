52173f061b3656443aa0b795f798f5227bb19411
Klaudia <klaudia.laskowska99@gmail.com>
2021-11-28 21:10:54 +0100

    Add adding and deleting follow on front

--- frontend-vue/src/components/social/profile/ProfileInfo.vue
+++ frontend-vue/src/components/social/profile/ProfileInfo.vue
@@ -14,7 +14,7 @@
                 <div class="text-start d-flex justify-content-between ms-3">
                     <h3>{{this.profileSource.firstName}} {{this.profileSource.lastName}}</h3>
 
-                    <button class="btn-white ms-auto fw-bolder" v-if="!this.$route.params.profileId || this.$route.params.profileId === this.$store.getters.getProfileId">
+                    <button class="btn-white ms-auto fw-bolder" v-if="isProfileMine">
                         <router-link :to="{ name: 'ProfileEdit' }">
                             Edytuj profil
                         </router-link>
@@ -28,9 +28,18 @@
                     <div class="p-2 mx-2 tag-nutrition fw-bolder" v-if="this.profileSource.enutritionTag !== 'NONE'">
                         {{ this.$func_global.mapNutritionTag(this.profileSource.enutritionTag) }}
                     </div>
+
                 </div>
 
             </div>
+        </div>
+        <div class="d-flex flex-row-reverse px-4 px-2">
+            <button class="btn-white" v-if="!isProfileMine && !this.isFollowedByMe" @click="addFollow">
+                Zaobserwuj
+            </button>
+            <button class="btn-white" v-else-if="!isProfileMine && this.isFollowedByMe" @click="deleteFollow">
+                Przestań obserwować
+            </button>
         </div>
         <div class="row text-start px-4 py-3">
             <p v-html="this.$func_global.convertNewLines(this.profileSource.description)"></p>
@@ -44,7 +53,8 @@ export default {
     name: "ProfileInfo",
     data() {
         return {
-            profilePictureSrc: ""
+            profilePictureSrc: "",
+            isFollowedByMe: false
         }
     },
     props: {
@@ -60,11 +70,48 @@ export default {
             const url = `${this.apiURL}profile/export/${profileId}`
             const token = this.$store.getters.getToken;
             this.$func_global.downloadPhoto(url, token).then(result => this.profilePictureSrc = result)
+        },
+        addFollow() {
+            const url = `${this.apiURL}follow/${this.$route.params.profileId}`
+            const token = this.$store.getters.getToken;
+            this.axios.post(url, null, {headers: {Authorization: `Bearer ${token}`}}).then((response) => {
+                console.log(response.data)
+                this.checkFollow()
+            }).catch(error => {
+                console.log(error.response.status)
+            });
+        },
+        deleteFollow() {
+            const url = `${this.apiURL}follow/${this.$route.params.profileId}`
+            const token = this.$store.getters.getToken;
+            this.axios.delete(url, {headers: {Authorization: `Bearer ${token}`}}).then((response) => {
+                console.log(response.data)
+                this.checkFollow()
+            }).catch(error => {
+                console.log(error.response.status)
+            });
+        },
+        checkFollow() {
+            const url = `${this.apiURL}follow/${this.$route.params.profileId}`
+            const token = this.$store.getters.getToken;
+            this.axios.get(url, {headers: {Authorization: `Bearer ${token}`}}).then((response) => {
+                console.log(response.data)
+                this.isFollowedByMe = response.data
+            }).catch(error => {
+                console.log(error.response.status)
+            });
+        }
+    },
+    computed: {
+        isProfileMine() {
+            return !this.$route.params.profileId || this.$route.params.profileId === this.$store.getters.getProfileId
         }
     },
     mounted() {
-        if(this.$route.params.profileId)
+        if(this.$route.params.profileId) {
             this.downloadProfilePictureByProfileId(this.$route.params.profileId)
+            this.checkFollow()
+        }
         else
             this.downloadMyProfilePicture()
     }
