501896e1c6a48fb461e9eaaedee7b95ca76400fb
kacperdubiel <kacper.dubiel13@gmail.com>
2021-11-30 08:38:29 +0100

    Added chart colors

--- frontend-vue/src/components/telemedic/analysis/AnalysisChartComponent.vue
+++ frontend-vue/src/components/telemedic/analysis/AnalysisChartComponent.vue
@@ -116,18 +116,18 @@ export default {
                         },
                         axisBorder: {
                             show: false,
-                            color: "#008FFB"
+                            color: "var(--TELEMEDIC-ANALYSIS-FIRST)"
                         },
                         labels: {
                             style: {
-                                colors: "#008FFB",
+                                colors: "var(--TELEMEDIC-ANALYSIS-FIRST)",
                             },
                             formatter: (val) => { return val.toFixed(1)},
                         },
                         title: {
                             text: "Series A",
                             style: {
-                                color: "#008FFB",
+                                color: "var(--TELEMEDIC-ANALYSIS-FIRST)",
                                 fontSize: '15px',
                                 fontWeight: 400,
                             }
@@ -142,18 +142,18 @@ export default {
                         },
                         axisBorder: {
                             show: false,
-                            color: "#00E396"
+                            color: "var(--TELEMEDIC-ANALYSIS-SECOND)"
                         },
                         labels: {
                             style: {
-                                colors: "#00E396"
+                                colors: "var(--TELEMEDIC-ANALYSIS-SECOND)"
                             },
                             formatter: (val) => { return val.toFixed(1)},
                         },
                         title: {
                             text: "Series B",
                             style: {
-                                color: "#00E396",
+                                color: "var(--TELEMEDIC-ANALYSIS-SECOND)",
                                 fontSize: '15px',
                                 fontWeight: 400,
                             }
@@ -168,18 +168,18 @@ export default {
                         },
                         axisBorder: {
                             show: false,
-                            color: "#FEB019"
+                            color: "var(--TELEMEDIC-ANALYSIS-THIRD)"
                         },
                         labels: {
                             style: {
-                                colors: "#FEB019"
+                                colors: "var(--TELEMEDIC-ANALYSIS-THIRD)"
                             },
                             formatter: (val) => { return val.toFixed(1)},
                         },
                         title: {
                             text: "Series C",
                             style: {
-                                color: "#FEB019",
+                                color: "var(--TELEMEDIC-ANALYSIS-THIRD)",
                                 fontSize: '15px',
                                 fontWeight: 400,
                             }
@@ -194,18 +194,18 @@ export default {
                         },
                         axisBorder: {
                             show: false,
-                            color: "#FF4560"
+                            color: "var(--TELEMEDIC-ANALYSIS-FOURTH)"
                         },
                         labels: {
                             style: {
-                                colors: "#FF4560"
+                                colors: "var(--TELEMEDIC-ANALYSIS-FOURTH)"
                             },
                             formatter: (val) => { return val.toFixed(1)},
                         },
                         title: {
                             text: "Series D",
                             style: {
-                                color: "#FF4560",
+                                color: "var(--TELEMEDIC-ANALYSIS-FOURTH)",
                                 fontSize: '15px',
                                 fontWeight: 400,
                             }
